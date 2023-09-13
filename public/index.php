b69a03968ef918b996372cd306b9a7299bf23335
Frederik Koza <fredekoza@gmail.com>
2023-09-13 13:12:05 +0200

    push

--- public/index.php
+++ public/index.php
@@ -5,19 +5,47 @@
     header("Access-Control-Max-Age: 3600"); 
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); 
     
-    require ".env";
+    require "./../.env";
 
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
 
+    $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+    $uri = explode( '/', $uri );
+    
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = getenv("PASSWORD"); // reads from the .env file (if imported in index.php)
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = getenv('PASSWORD');
     try {
-        $conn = new PDO('mysql:host=$servername;dbname=pipper', $username, $password);
-
+        $conn = new PDO("mysql:host=$servername;dbname=pipper", $username, $password);
+        // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Connected successfully';
-    } catch (PDOException $e) {
-    echo 'Connection failed:' . $e->getMessage();
+        
+        if ($requestMethod == "GET") {
+
+            if ($uri[1] == "pipper") {
+            }
+            $statement = $conn->query("select * from pipper");
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            echo json_encode($result);    
+        } else if ($requestMethod == "POST" && $uri[1] == "pipper") {
+            // echo "You sent a POST request, so you get no data, hahaha!";
+        }
+
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        echo $input ['text'];
+        echo $input ['username'];
+        
+        
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
-?>
+
+    if ($uri[1] == "pipper") {
+        
+    }
+
+    
+    // echo "Hello world"; 
+?> 
